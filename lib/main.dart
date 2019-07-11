bd3381656bca3f0b7e3b6689e9a56784713370ca
王資猛 <wangzimeng@wangzimengde-MacBook-Pro.local>
2019-07-11 17:45:06 +0800

    first look at container

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,8 @@
 import 'package:flutter/material.dart';
-import './app_screens/first_screen.dart';
+import './app_screens/home.dart';
 
-void main()=> runApp(MyFlutterApp());
-
-
-class MyFlutterApp extends StatelessWidget{
-  @override
-  Widget build(BuildContext context){
-    return   MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title:'Regroup',
-      home:
-      Scaffold(
-        appBar: AppBar(title: Text('Comes')),
-        body: FirstScreen()      
-      )
-
-    );
-  }
-}
+void main()=> runApp(MaterialApp(
+  title: 'Exploring UI widget',
+  home: Home(),
+)
+);
