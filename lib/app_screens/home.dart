446091304eeec07012847e2dc5d064a58542eba4
王資猛 <wangzimeng@wangzimengde-MacBook-Pro.local>
2019-07-12 11:17:24 +0800

    add column and row

--- lib/app_screens/home.dart
+++ lib/app_screens/home.dart
@@ -5,12 +5,82 @@ class Home extends StatelessWidget{
   Widget build(BuildContext context) {
     return Center(
       child:  Container(
+      padding: EdgeInsets.only(left: 10.0, top: 40.0),
       alignment: Alignment.center,
       color: Colors.deepPurple,
 //      width: 200.0,
 //      height: 100.0,
       margin: EdgeInsets.all(15.0),
-      child: Text("Flight", textDirection: TextDirection.ltr),
+      child: Column(
+        children: <Widget>[
+      Row(
+        children: <Widget>[
+        Expanded(child: Text(
+        "Flight",
+         textDirection: TextDirection.ltr,
+         style: TextStyle(
+         decoration: TextDecoration.none,
+         fontSize: 30.0
+         )
+         ),
+        ),
+        Expanded(child: Text(
+        "Boat",
+         textDirection: TextDirection.ltr,
+         style: TextStyle(
+         decoration: TextDecoration.none,
+         fontSize: 20.0
+         )
+         ),
+        ),
+          Expanded(child: Text(
+        "Car",
+         textDirection: TextDirection.ltr,
+         style: TextStyle(
+         decoration: TextDecoration.none
+         )
+         ),
+          ),
+        ],
+      ),
+      Row(
+        children: <Widget>[
+        Expanded(child: Text(
+        "Flight",
+         textDirection: TextDirection.ltr,
+         style: TextStyle(
+         decoration: TextDecoration.none,
+         fontSize: 35.0
+         )
+         ),
+        ),
+        Expanded(child: Text(
+        "Boat",
+         textDirection: TextDirection.ltr,
+         style: TextStyle(
+         decoration: TextDecoration.none
+         )
+         ),
+        ),
+          Expanded(child: Text(
+        "Car",
+         textDirection: TextDirection.ltr,
+         style: TextStyle(
+         decoration: TextDecoration.none
+         )
+         ),
+          ),
+        ],
+      )
+        ],
+      )
+      
+      
+      
+
+      
+
+
     ) 
     );
     
